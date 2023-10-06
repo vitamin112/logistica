6b7af915b5b82fbdd54bff258fa344230a48289c
Nguyễn Văn Lộc <88890670+vitamin112@users.noreply.github.com>
2023-10-06 08:23:16 +0700

    config view

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,14 +5,14 @@ const router = express.Router();
 
 const initWebRouter = (app) => {
   router.get("/user/create-user", homeController.createNewUser);
-  router.get("/trash", homeController.showTrash);
-  router.get("/update-user/:id", homeController.getUserToUpdate);
   router.post("/create-user", homeController.handleCreateNewUser);
+  router.get("/update-user/:id", homeController.getUserToUpdate);
+  router.post("/user/update-user", homeController.handleUpdateUser);
   router.post("/restore/:id", homeController.handleRestore);
   router.post("/user/delete-user/:id", homeController.handleDeleteUser);
   router.post("/user/destroy/:id", homeController.handleDestroyUser);
-  router.post("/user/update-user", homeController.handleUpdateUser);
   router.get("/user", homeController.showPagination);
+  router.get("/trash", homeController.showTrash);
   router.get("/", homeController.index);
 
   return app.use("/", router);
